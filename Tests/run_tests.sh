8421bd947490c74770ddd5fe4e3c10637bc22a92
Nuwantha <nw@me.loc>
2017-06-23 15:40:45 +0200

    update autotest script

--- Tests/run_tests.sh
+++ Tests/run_tests.sh
@@ -4,9 +4,21 @@
 # sudo cp Tests/phpunit-5.2.phar /usr/bin/phpunit
 # sudo chmod 0777 /usr/bin/phpunit
 
-phpunit --bootstrap vendor/autoload.php Tests/StorageTest.php
+echo "StorageTest";
+phpunit --bootstrap vendor/autoload.php Tests/StorageTest
+
+echo "TableSchemaTest";
 phpunit --bootstrap vendor/autoload.php Tests/TableSchemaTest
+
+echo "IndexTest";
 phpunit --bootstrap vendor/autoload.php Tests/IndexTest
+
+echo "SelectTest";
 phpunit --bootstrap vendor/autoload.php Tests/SelectTest
+
+echo "UpdateTest";
 phpunit --bootstrap vendor/autoload.php Tests/UpdateTest
+
+echo "DeleteTest";
 phpunit --bootstrap vendor/autoload.php Tests/DeleteTest
+
